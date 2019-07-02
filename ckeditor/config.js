2cdcd0d13a6281c0d2ef0a5b135a3786dbcc4fc4
Dezys <49292466+Dezys@users.noreply.github.com>
2019-07-02 17:07:21 +0300

    Problem with editor's height somehow was solved

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -10,7 +10,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.resize_enabled = false;
 	config.language = 'ru';
 	config.uiColor = '#ffffff';
-	config.height = '80vh';
+	config.height = '90vh';
 	config.toolbarLocation = 'bottom';
 	config.startupMode = 'wysiwyg';
 };
